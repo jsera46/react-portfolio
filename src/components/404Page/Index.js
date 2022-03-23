2f59616f0e2408b6fdddaa776967c5426eebb49f
jsera46 <jsera46@gmail.com>
2022-03-23 16:54:36 +0800

    still working on 404 page

--- src/components/404Page/Index.js
+++ src/components/404Page/Index.js
@@ -1,16 +1,41 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 import "../../styles/app.css";
-import { Footer, Navbar } from "../Exporter";
+import { Navbar } from "../Exporter";
 
 const Index = () => {
   const location = useLocation();
   location.pathname = "/404";
+
   return (
     <>
-      <Navbar />
-      404Page Component
-      <Footer />
+    <Navbar />
+    <div className="div404">
+      <div className="col-md-12">
+        {/* linkDiv */}
+        <div className="col-md-6 linkDiv">
+          {/* Bar */}
+          <div className="first-bar"></div>
+
+          {/* Icons */}
+          <div className="iconsDiv">
+            <a href="#"><i class="fab fa-github"></i></a>
+            <a href="#"><i class="fab fa-linkedin"></i></a>
+            <a href="#"><i class="fab fa-facebook"></i></a>
+          </div>
+          {/* 2nd Bar */}
+          <div className="second-bar"></div>
+        </div>
+
+        {/*  */}
+        {/* <div className="col-md-6">
+          <div className="contentClass" >
+            <label>404</label>
+            <label>Woops... Sorry, we are not able to find what you where looking for</label>
+          </div>
+        </div> */}
+      </div>
+    </div>
     </>
   );
 };
