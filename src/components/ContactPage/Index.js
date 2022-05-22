79b2ff75326c47f421aed5e1deb160445f8d6324
jsera46 <jsera46@gmail.com>
2022-05-22 20:11:52 +0800

    in landing I finish the task in the contact component, for the background image i use inline css and rest was in sass file

--- src/components/ContactPage/Index.js
+++ src/components/ContactPage/Index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "../../styles/app.css";
-import { Footer, Navbar } from "../Exporter";
 
 const Index = () => <>ContactPage Component</>;
 
--- src/components/LandingPage/LandingPageContact/LandingPageContact.js
+++ src/components/LandingPage/LandingPageContact/LandingPageContact.js
@@ -1,10 +1,22 @@
 import React from "react";
 import "../../../styles/app.css";
+import background from "../../../assets/landing/contact.png";
 
 const LandingPageContact = () => {
   return (
-    <div className="landing-contact-container">
-      <span>Contact</span>
+    <div
+      className="landing-contact-container"
+      style={{ backgroundImage: `url(${background})` }}
+    >
+      <div className="left-div">
+        <span>“A successful website does three things:</span>
+        <span>It attracts the right kinds of visitors.</span>
+        <span>Guides them to the main services or product you offer.</span>
+        <span>Collect Contract details for future ongoing relation. “</span>
+      </div>
+      <div className="right-div">
+        <button type="button">contact</button>
+      </div>
     </div>
   );
 };
